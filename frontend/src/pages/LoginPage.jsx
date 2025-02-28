67ed077762fea46542c49e135c8e182d07b94578
Vernere <Verner.etola@metropolia.fi>
2025-02-28 11:25:23 +0200

    Iteration4

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,29 +1,7 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import useLogin from '../hooks/useLogin';
 
 const LoginPage = ({ loginSubmit }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const navigate = useNavigate();
-
-    const submitForm = async (e) => {
-        e.preventDefault();
-
-        const loginData = {
-            email,
-            password,
-        };
-
-        try {
-            await loginSubmit(loginData);
-            toast.success('Login Successful');
-            navigate('/');
-        } catch (error) {
-            toast.error(error.message || 'Login failed');
-        }
-    };
+    const { email, setEmail, password, setPassword, submitForm } = useLogin(loginSubmit);
 
     return (
         <section className='bg-indigo-50'>
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import useSignup from '../hooks/useSignup';
 
 const RegisterPage = ({ registerSubmit }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-
-    const navigate = useNavigate();
-
-    const submitForm = async (e) => {
-        e.preventDefault();
-
-        if (password !== confirmPassword) {
-            toast.error('Passwords do not match');
-            return;
-        }
-
-        const registerData = {
-            email,
-            password,
-        };
-
-        try {
-            await registerSubmit(registerData);
-            toast.success('Registration Successful');
-            navigate('/login');
-        } catch (error) {
-            toast.error(error.message || 'Registration failed');
-        }
-    };
+    const {
+        email,
+        setEmail,
+        password,
+        setPassword,
+        confirmPassword,
+        setConfirmPassword,
+        submitForm,
+    } = useSignup(registerSubmit);
 
     return (
         <section className='bg-indigo-50'>
