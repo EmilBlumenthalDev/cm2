6878c54546c3585927cb575cea4237cd2c769fa3
Vernere <Verner.etola@metropolia.fi>
2025-02-28 19:49:19 +0200

    No auth required on getAllJobs

--- backend/api-server-starter/routes/jobRouter.js
+++ backend/api-server-starter/routes/jobRouter.js
@@ -6,13 +6,12 @@ const { createJob, getAllJobs, getJob, editJob, deleteJobById } = require('../co
 const requireAuth = require('../middleware/requireAuth');
 
 const router = express.Router();
-
+router.get('/', getAllJobs);
 // apply the requireAuth middleware to all routes
 router.use(requireAuth);
 
 // create job route
 router.post('/', createJob);
-router.get('/', getAllJobs);
 router.get('/:id', getJob);
 router.put('/:id', editJob); // edit job route
 router.delete('/:jobId', deleteJobById);
