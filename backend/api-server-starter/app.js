4c37b2c1c097eb4e53295653b59ac38994184b67
Vernere <Verner.etola@metropolia.fi>
2025-02-28 17:36:29 +0200

    dist preview build

--- backend/api-server-starter/app.js
+++ backend/api-server-starter/app.js
@@ -12,12 +12,17 @@ const cors = require("cors");
 app.use(cors())
 app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(express.static('view')); // Serve static assets
 connectDB();
 
 // Use the userRouter for all /users routes
 app.use("/api/users", userRouter);
 app.use("/api/jobs", jobRouter);
+
+// Handle all other routes with the React app
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/view/index.html');
+});
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
