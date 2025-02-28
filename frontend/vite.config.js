cbe8a64725ed60df6a449c2ae57bfdee32c4218f
Vernere <Verner.etola@metropolia.fi>
2025-02-28 16:28:53 +0200

    config changed to deployed backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://cm2-q627.onrender.com/",
         changeOrigin: true,
       },
     },
